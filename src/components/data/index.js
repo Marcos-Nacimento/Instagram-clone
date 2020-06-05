2a1953f1161410e45a1224e5e5da5faea7e2fe68
Marcos-Nacimento <62677231+Marcos-Nacimento@users.noreply.github.com>
2020-06-04 17:03:15 -0700

    Update index.js

--- src/components/data/index.js
+++ src/components/data/index.js
@@ -1,101 +1,103 @@
-export const users = [
+export const database = 
+  [
+    // posts
     {
-      id: "derek.russel",
-      name: "Derek Russel",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/8bbac5ad06b4a569b8a446825f7371c81ebac821.png?alt=media&token=73b33332-c587-464b-af68-52554221b73a",
-      caption: "",
+      id: '75',
+      post: 'https://jornadakamoi.files.wordpress.com/2014/10/img_9078.jpg'
     },
     {
-      id: "jmitch",
-      name: "Joe Mitchell",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/9b5cb1a55d786aebebfe7379dc16b8ccf0e81942.png?alt=media&token=6f3c5b07-1b67-478b-9b46-15cfee8d6f54",
-      caption: "",
+      id: '42',
+      post: 'https://ak.picdn.net/shutterstock/videos/9727475/thumb/1.jpg'
     },
     {
-      id: "monicaa",
-      name: "Monica Dixon",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/a71de5589d604ece4a685c2c270267cebe192be8.png?alt=media&token=0d91856a-8a43-4b58-8e44-57becc3f34eb",
-      caption: "",
+      id: '41',
+      post: 'https://i.pinimg.com/originals/b1/f2/0e/b1f20e60bee76a2ac38df2c36be25235.jpg'
     },
     {
-      id: "andrea.schmidt",
-      name: "Andrea Schmidt",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/6e37e1c924aa55072f25e47ede573285a6fb69bf.png?alt=media&token=ae4e9186-91b8-4748-963a-02138a3e395c",
-      caption: "",
+      id: '52',
+      post: 'https://i.pinimg.com/originals/68/d4/77/68d477293ad1729027bc7da7a4bbfa4c.jpg'
     },
     {
-      id: "alexandergarcia",
-      name: "Alex Garcia",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/a2c02707b8423da62b591e7111e790a77917cf14.png?alt=media&token=98e89375-36ce-4477-b19a-7d68194de3e8",
-      caption: "",
+      id: '15',
+      post: 'https://www.gazetaesportiva.com/wp-content/uploads/imagem/2020/04/07/EPxx4hJWAAAMS-M-1024x674.jpg'
     },
     {
-      id: "mjcole",
-      name: "Michael Cole",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/40eabfbf1c4cd057304d9a18e260d42c7025b5ca.png?alt=media&token=ebbc0a41-61db-4bed-a0c5-8f088eb16c21",
-      caption: "",
+      id: '5',
+      post: 'https://media.glassdoor.com/l/63/35/7d/bb/thompson-island-4k.jpg'
     },
-    {
-      id: "schavez",
-      name: "Sylvia Chavez",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/6e37e1c924aa55072f25e47ede573285a6fb69bf.png?alt=media&token=ae4e9186-91b8-4748-963a-02138a3e395c",
-      caption: "Aspiring creative writer. I love 🌶 food and good people.",
-      cover: {
-        uri:
-          "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/e88d0539c04f38d3b7f7d030779f11feb26c3660.png?alt=media&token=7d2041fa-ed1f-41ee-ad6a-8af1531d657b",
-        preview:
-          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAAAXNSR0IArs4c6QAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAACXBIWXMAAAsTAAALEwEAmpwYAAABWWlUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iWE1QIENvcmUgNS40LjAiPgogICA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogICAgICA8cmRmOkRlc2NyaXB0aW9uIHJkZjphYm91dD0iIgogICAgICAgICAgICB4bWxuczp0aWZmPSJodHRwOi8vbnMuYWRvYmUuY29tL3RpZmYvMS4wLyI+CiAgICAgICAgIDx0aWZmOk9yaWVudGF0aW9uPjE8L3RpZmY6T3JpZW50YXRpb24+CiAgICAgIDwvcmRmOkRlc2NyaXB0aW9uPgogICA8L3JkZjpSREY+CjwveDp4bXBtZXRhPgpMwidZAAAA8ElEQVQYGRWPS1LCQABEX2YmwyR8EmRheRHv4Vm8rUstsQoKMAbI/Mew7a5+3V29vr+VKxa5KmSRmI6J4gSls6QQSHvB/X5H9M8Na9MQfuB8vlG2Gd0I5K+EKpONx/95VJSebqtnsWC/J1x50CFOkTBm4tpDn1Ff+4FWStKiUK0q4ikwVhEnA3FISJ9JtUe8mJ48FcIlUhfY7TYsQv2oousNEoWUGuWspVE1q7wgztRjHphyoNdLyrzoGiY27RJxGE/E+Z3QEpcd++FI6yW6NVxxSAvqpmZ/junaYHXg4/RJf1Y8dVucCUyHEWMNfj72D6rqgcOmcLiwAAAAAElFTkSuQmCC",
-      },
+    
+  ]
+
+export const users = 
+[
+  // users
+  {
+    id: "79",
+    name: "Mateus",
+    user:
+      "https://http2.mlstatic.com/foto-desenho-para-o-seu-perfil-das-redes-sociais-D_NQ_NP_614041-MLB27185740295_042018-O.jpg",
+    caption: "",
+  },
+  {
+    id: "48",
+    name: "Joice",
+    user:
+      "https://www.dicasdemulher.com.br/wp-content/uploads/2019/06/legenda-para-foto-de-perfil-1200x738.jpg",
+    caption: "",
+  },
+  {
+    id: "26",
+    name: "Carlos",
+    user:
+      "https://images.squarespace-cdn.com/content/v1/51435924e4b02285c8b9c92d/1435276997644-5WE6FAVOB6Y9HZH89Y7A/ke17ZwdGBToddI8pDm48kNBhxsR5AixTPaSt36FQjZRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIHEpb-MmdDNvFVgjmeoENIlexef176In2EgYPtI8R2-8KMshLAGzx4R3EDFOm1kBS/caiobraga-perfil.jpg",
+    caption: "",
+  },
+  {
+    id: "24",
+    name: "Lucas",
+    user:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIH8Woz_16Y8WH0qSKj0IuXmLFHc7zeSGx-7tbMzZbv-iVYo_d&usqp=CAU",
+    caption: "",
+  },
+  {
+    id: "31",
+    name: "Messi",
+    user:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ7g2_kc23lxiO2DI9fQexDiNuwum5kJQzHlJ_48GW4PUeNCoWn&usqp=CAU",
+    caption: "",
+  },
+  {
+    id: "89",
+    name: "Maria",
+    user:
+      "https://i.pinimg.com/originals/9c/1f/42/9c1f425a3745f63bd0bbb48f160ebf7a.jpg",
+    caption: "",
+  },
+  {
+    id: "105",
+    name: "Alice",
+    user:
+      "https://www.dicasdemulher.com.br/wp-content/uploads/2019/06/legenda-para-foto-de-perfil-03.jpg",
+    caption: "Aspiring creative writer. I love 🌶 food and good people.",
+    cover: {
+      uri:
+        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/e88d0539c04f38d3b7f7d030779f11feb26c3660.png?alt=media&token=7d2041fa-ed1f-41ee-ad6a-8af1531d657b",
+      preview:
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAAAXNSR0IArs4c6QAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAACXBIWXMAAAsTAAALEwEAmpwYAAABWWlUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iWE1QIENvcmUgNS40LjAiPgogICA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogICAgICA8cmRmOkRlc2NyaXB0aW9uIHJkZjphYm91dD0iIgogICAgICAgICAgICB4bWxuczp0aWZmPSJodHRwOi8vbnMuYWRvYmUuY29tL3RpZmYvMS4wLyI+CiAgICAgICAgIDx0aWZmOk9yaWVudGF0aW9uPjE8L3RpZmY6T3JpZW50YXRpb24+CiAgICAgIDwvcmRmOkRlc2NyaXB0aW9uPgogICA8L3JkZjpSREY+CjwveDp4bXBtZXRhPgpMwidZAAAA8ElEQVQYGRWPS1LCQABEX2YmwyR8EmRheRHv4Vm8rUstsQoKMAbI/Mew7a5+3V29vr+VKxa5KmSRmI6J4gSls6QQSHvB/X5H9M8Na9MQfuB8vlG2Gd0I5K+EKpONx/95VJSebqtnsWC/J1x50CFOkTBm4tpDn1Ff+4FWStKiUK0q4ikwVhEnA3FISJ9JtUe8mJ48FcIlUhfY7TYsQv2oousNEoWUGuWspVE1q7wgztRjHphyoNdLyrzoGiY27RJxGE/E+Z3QEpcd++FI6yW6NVxxSAvqpmZ/junaYHXg4/RJf1Y8dVucCUyHEWMNfj72D6rqgcOmcLiwAAAAAElFTkSuQmCC",
     },
-    {
-      id: "griffinsof",
-      name: "Sofia Griffin",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/8d8edc4c628969721a3127824566bcf7bef353c9.png?alt=media&token=99e98755-78ec-439b-ac51-778cba2d2cda",
-      caption: "",
-    },
-    {
-      id: "itsmesuri",
-      name: "Suri H.",
-      picture:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/881c9bfb4e5d3578355f328be01fe1d5152bbb27.png?alt=media&token=5ebcebcd-0c90-4d9d-8c45-daa05e6f1f41",
-      caption: "",
-    },
-  ];
-  
-  export const posts = [
-    {
-      id: "ffbeb717-9ffb-4a83-9b71-d47a90b57f60",
-      user: "andrea.schmidt",
-      picture: {
-        uri:
-          "https://firebasestorage.googleapis.com/v0/b/react-native-ui-kits.appspot.com/o/assets%2Fzurich1.jpg?alt=media&token=8c900be1-b6d6-427d-9739-1f637d8369e6",
-      },
-      caption: "🇨🇭 Beautiful Zürich Switzerland",
-      timestamp: 1514764800,
-      comments: ["andrea.schmidt", "monicaa", "alexandergarcia", "jmitch"],
-      likes: 42,
-      avatar:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/6e37e1c924aa55072f25e47ede573285a6fb69bf.png?alt=media&token=ae4e9186-91b8-4748-963a-02138a3e395c",
-    },
-    {
-      id: "f5f09829-951e-4d32-a400-0c6a097dd29d",
-      user: "monicaa",
-      picture: {
-        uri:
-          "https://firebasestorage.googleapis.com/v0/b/react-native-ui-kits.appspot.com/o/assets%2Fzurich2.jpg?alt=media&token=b213731b-d54b-4072-8268-52f326b635e9",
-      },
-      caption: "Finding Balance",
-      timestamp: 1514769900,
-      likes: 24,
-      avatar:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-e.appspot.com/o/a71de5589d604ece4a685c2c270267cebe192be8.png?alt=media&token=0d91856a-8a43-4b58-8e44-57becc3f34eb",
-    },
-  ];
+  },
+  {
+    id: "29",
+    name: "Caio",
+    user:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTAwyWVknkyOhfS_1aCg2ecbXy-9TkeHiDYioOOJz5-0v30r3nY&usqp=CAU",
+    caption: "",
+  },
+  {
+    id: "18",
+    name: "Will",
+    user:
+      "https://amofamilia.com.br/wp-content/uploads/2019/05/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+    caption: "",
+  },
+]
